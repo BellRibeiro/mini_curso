282238261a416689c90c9300aef0975ef21ca93a
BellRibeiro <anacibely1818@gmail.com>
2024-05-25 09:43:07 -0300

    main

---
+++ app/Models/Aluno.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Aluno extends Model
+{
+    protected $fillable=['nome', 'cpf'];
+
+public function disciplina(){
+     return $this->belongsToMany(Disciplina::class,'aluno_disciplina');
+}
+
+    use HasFactory;
+}
